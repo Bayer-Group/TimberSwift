47e15c319915d2d435b2724eaba2362787900c38
Vernon Schierding <schlingding@gmail.com>
2018-12-10 10:03:06 -0600

    Added an identifier to Performance Trace for tracking multiple asynchronous calls. API breaking changes in the TimberApplicationDelegate and deprecation warnings in Timber.Performance.

--- Example/iOSExample/AppSession.swift
+++ Example/iOSExample/AppSession.swift
@@ -7,21 +7,42 @@ final class AppSession {
     let timber: TimberProtocol
     private let source = Source(title: "Example Application", version: "1.2.3", emoji: Character("🌲"))
     
+    private var performanceTime: [String: Date] = [:]
+    private let timer: DispatchSourceTimer
+    
     private var currentNetworkCalls = 0 {
         didSet {
             DispatchQueue.main.async { [unowned self] in
-                if self.currentNetworkCalls > 0 {
-                    UIApplication.shared.isNetworkActivityIndicatorVisible = true
-                } else {
-                    UIApplication.shared.isNetworkActivityIndicatorVisible = false
-                }
+                UIApplication.shared.isNetworkActivityIndicatorVisible = self.currentNetworkCalls > 0
             }
         }
     }
     
     private init() {
         timber = Timber(source: source)
+        
+        let performanceOvertimeCheckInSeconds: Double = 60
+        let performanceOvertimeMaximumInSeconds: Double = 60 * 30
+        
+        let queue = DispatchQueue(label: "timber.performance.overtime.timer")
+        timer = DispatchSource.makeTimerSource(queue: queue)
+        timer.schedule(deadline: .now() + performanceOvertimeCheckInSeconds, repeating: performanceOvertimeCheckInSeconds, leeway: .seconds(0))
+        timer.setEventHandler { [weak self] in
+            let overtimeValues: [String]? = self?.performanceTime.compactMap {
+                let secondsElapsed = Date().timeIntervalSince1970 - $0.value.timeIntervalSince1970
+                return secondsElapsed > performanceOvertimeMaximumInSeconds ? $0.key : nil
+            }
+            overtimeValues?.forEach {
+                self?.performanceTime[$0] = nil
+            }
+        }
+        timer.resume()
+        
         Timber.timberApplicationDelegate = self
+    }
+    
+    deinit {
+        timer.cancel()
     }
 }
 
@@ -46,16 +67,24 @@ extension AppSession: TimberApplicationDelegate {
         print("TOAST: Parent should display toast message: \"\(message)\", displayTime: \(displayTime), type: \(type), source: \"\(source)\"")
     }
     
-    func startTrace(key: String, properties: [String: Any]?, source: Source) {
-        print("TRACE: Start performance trace key: \(key), properties: \(properties ?? [:]), source: \"\(source)\"")
+    func startTrace(key: String, identifier: UUID?, properties: [String: Any]?, source: Source) {
+        performanceTime[key.performanceTimeKey(identifier: identifier)] = Date()
+        
+        print("TRACE: Start performance trace key: \(key), identifier: \(identifier?.uuidString ?? ""), properties: \(properties ?? [:]), source: \"\(source)\"")
     }
     
-    func incrementTraceCounter(key: String, named: String, by count: Int, source: Source) {
-        print("TRACE: Increment performance trace counter key: \(key), name: \(named), by: \(count), source: \"\(source)\"")
+    func incrementTraceCounter(key: String, identifier: UUID?, named: String, by count: Int, source: Source) {
+        let startDate = performanceTime[key.performanceTimeKey(identifier: identifier)]
+        let timeElapsed = Date().timeIntervalSince1970 - (startDate?.timeIntervalSince1970 ?? Date().timeIntervalSince1970)
+        
+        print("TRACE: Increment performance trace counter key: \(key), identifier: \(identifier?.uuidString ?? ""), name: \(named), by: \(count), timeElapsed: \(timeElapsed), source: \"\(source)\"")
     }
     
-    func stopTrace(key: String, source: Source) {
-        print("TRACE: Stop performance trace key: \(key), source: \"\(source)\"")
+    func stopTrace(key: String, identifier: UUID?, source: Source) {
+        let startDate = performanceTime.removeValue(forKey: key.performanceTimeKey(identifier: identifier))
+        let timeElapsed = Date().timeIntervalSince1970 - (startDate?.timeIntervalSince1970 ?? Date().timeIntervalSince1970)
+        
+        print("TRACE: Stop performance trace key: \(key), identifier: \(identifier?.uuidString ?? ""), timeElapsed: \(timeElapsed), source: \"\(source)\"")
     }
     
     func networkActivityStarted(source: Source) {
@@ -66,5 +95,13 @@ extension AppSession: TimberApplicationDelegate {
     func networkActivityEnded(source: Source) {
         print("NETWORK: Activity ended from source: \"\(source)\"")
         currentNetworkCalls -= 1
+    }
+}
+
+fileprivate extension String {
+    func performanceTimeKey(identifier: UUID?) -> String {
+        guard let identifier = identifier?.uuidString else { return self }
+        
+        return "\(self)|\(identifier)"
     }
 }
--- Sources/TimberSwift/Performance/Performance.swift
+++ Sources/TimberSwift/Performance/Performance.swift
@@ -1,44 +1,66 @@
+import Foundation
+
 public protocol PerformanceProtocol {
     /**
      Start a performance trace which is intended to measure the time between start and stop
      - parameter title: The title with expected format of space seperated capitalized words.
+     - parameter identifier: The unique identifier for this trace.
      - parameter properties: Supplementary trace key/value pairs. The key is expected to be space seperated capitalized words.
      */
-    func startTrace(key: String, properties: [String: Any]?)
+    func startTrace(key: String, identifier: UUID?, properties: [String: Any]?)
     
     /**
      Increment a performance trace by increment name and amount
      - parameter key: The key of the trace
+     - parameter identifier: The unique identifier for this trace.
      - parameter named: The name of the increment
      - parameter count: The amount to increment
      */
-    func incrementTraceCounter(key: String, named: String, by count: Int)
+    func incrementTraceCounter(key: String, identifier: UUID?, named: String, by count: Int)
     
     /**
      Stop a performance trace
      - parameter title: The title with expected format of space seperated capitalized words.
+     - parameter identifier: The unique identifier for this trace.
      */
-    func stopTrace(key: String)
+    func stopTrace(key: String, identifier: UUID?)
+}
+
+public extension PerformanceProtocol {
+    @available(*, deprecated, message: "A UUID Identifier is now part of the signature and this will be removed in a future version")
+    public func startTrace(key: String, properties: [String: Any]?) {
+        return startTrace(key: key, identifier: nil, properties: properties)
+    }
+    
+    @available(*, deprecated, message: "A UUID Identifier is now part of the signature and this will be removed in a future version")
+    public func incrementTraceCounter(key: String, named: String, by count: Int) {
+        incrementTraceCounter(key: key, identifier: nil, named: named, by: count)
+    }
+    
+    @available(*, deprecated, message: "A UUID Identifier is now part of the signature and this will be removed in a future version")
+    public func stopTrace(key: String) {
+        stopTrace(key: key, identifier: nil)
+    }
 }
 
 internal protocol PerformanceDelegate: class {
-    func startTrace(key: String, properties: [String: Any]?)
-    func incrementTraceCounter(key: String, named: String, by count: Int)
-    func stopTrace(key: String)
+    func startTrace(key: String, identifier: UUID?, properties: [String: Any]?)
+    func incrementTraceCounter(key: String, identifier: UUID?, named: String, by count: Int)
+    func stopTrace(key: String, identifier: UUID?)
 }
 
 internal class Performance: PerformanceProtocol {
     weak var performanceDelegate: PerformanceDelegate?
     
-    func startTrace(key: String, properties: [String: Any]?) {
-        performanceDelegate?.startTrace(key: key, properties: properties)
+    func startTrace(key: String, identifier: UUID?, properties: [String: Any]?) {
+        performanceDelegate?.startTrace(key: key, identifier: identifier, properties: properties)
     }
     
-    func incrementTraceCounter(key: String, named: String, by count: Int) {
-        performanceDelegate?.incrementTraceCounter(key: key, named: named, by: count)
+    func incrementTraceCounter(key: String, identifier: UUID?, named: String, by count: Int) {
+        performanceDelegate?.incrementTraceCounter(key: key, identifier: identifier, named: named, by: count)
     }
     
-    func stopTrace(key: String) {
-        performanceDelegate?.stopTrace(key: key)
+    func stopTrace(key: String, identifier: UUID?) {
+        performanceDelegate?.stopTrace(key: key, identifier: identifier)
     }
 }
